3ca200622dd5d4f5b5d987aa7b50d9b324ed6e7e
André Nascimento <andrevsn1@gmail.com>
2026-09-03 16:02:15 -0300

    create new scripts

---
+++ cut_data_SSPARQ/gen_catalog.py
@@ -0,0 +1,14 @@
+from obspy import UTCDateTime
+from obspy.clients.fdsn import Client
+
+client = Client("USGS")
+
+#starttime = UTCDateTime("2025-09-25T00:00:00")
+#endtime = UTCDateTime("2026-09-03T00:00:00")
+starttime = UTCDateTime("2025-07-29T00:00:00")
+endtime = UTCDateTime("2025-08-04T00:00:00")
+minmag = 5.5
+
+catalog = client.get_events(starttime=starttime, endtime=endtime, minmagnitude=minmag)
+
+catalog.write("NEIC_USGS_2025_2026.xml", format="QUAKEML")
